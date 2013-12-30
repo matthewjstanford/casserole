c2261d92ce15191248a92f51c4e35d43a5ccbff7
Matthew Stanford <matthewjstanford@gmail.com>
2013-12-30 13:55:40 -0500

    Depends on yum 3.0, repo recipe updated accordingly

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       "Installs/Configures Apache Cassandra"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version           "0.2.0"
 
-depends           "yum"
+depends           "yum", "~> 3.0"
 depends           "apt"
 depends           "java", "~> 1.15"
 depends           "sysctl"
