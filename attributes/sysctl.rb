a06bc37e52daef69613857a4aae8bbe7bb1d7e73
Matthew Stanford <matthewjstanford@gmail.com>
2014-01-27 22:46:34 -0500

    Removed the RHEL default sysctl settings, they will stay in sysctl.conf

--- attributes/sysctl.rb
+++ attributes/sysctl.rb
@@ -18,20 +18,3 @@ default['sysctl']['params']['vm']['min_free_kbytes']                = 65536     
 default['sysctl']['params']['net']['ipv4']['tcp_window_scaling']    = 1                   # should be enabled by default
 default['sysctl']['params']['net']['ipv4']['tcp_max_syn_backlog']   = 2048
 
-# RedHat default settings:
-default['sysctl']['params']['net']['bridge']['bridge-nf-call-ip6tables'] = 0              # Disable netfilter on bridges.
-default['sysctl']['params']['net']['bridge']['bridge-nf-call-iptables'] = 0               # Disable netfilter on bridges.
-default['sysctl']['params']['net']['bridge']['bridge-nf-call-arptables'] = 0              # Disable netfilter on bridges.
-default['sysctl']['params']['net']['ipv4']['conf']['default']['accept_source_route'] = 0  # Do not accept source routing
-default['sysctl']['params']['net']['ipv4']['conf']['default']['rp_filter'] = 1            # Controls source route verification
-default['sysctl']['params']['net']['ipv4']['ip_forward'] = 0                              # Controls IP packet forwarding
-default['sysctl']['params']['net']['ipv4']['tcp_syncookies'] = 1                          # Controls the use of TCP syncookies
-default['sysctl']['params']['kernel']['core_uses_pid'] = 1                                # Controls whether core dumps will append the PID to the core filename.
-default['sysctl']['params']['kernel']['msgmnb'] = 65536                                   # Controls the default maxmimum size of a mesage queue
-default['sysctl']['params']['kernel']['msgmax'] = 65536                                   # Controls the maximum size of a message, in bytes
-default['sysctl']['params']['kernel']['printk'] = [8, 4, 1, 7]                            # Maximize console logging level for kernel printk messages
-default['sysctl']['params']['kernel']['printk_ratelimit'] = 5                             # Maximize console logging level for kernel printk messages
-default['sysctl']['params']['kernel']['printk_ratelimit_burst'] = 10                      # Maximize console logging level for kernel printk messages
-default['sysctl']['params']['kernel']['shmmax'] = 68719476736                             # Controls the maximum shared segment size, in bytes
-default['sysctl']['params']['kernel']['shmall'] = 4294967296                              # Controls the maximum number of shared memory segments, in pages
-default['sysctl']['params']['kernel']['sysrq'] = 0                                        # Controls the System Request debugging functionality of the kernel
