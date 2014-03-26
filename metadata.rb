e6e40806fa1d309313512134aab145b772ae66a9
Matthew Stanford <matthewjstanford@gmail.com>
2014-03-26 12:50:44 -0400

    Updated package versions

--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer_email  "j@p4nt5.com"
 license           "Apache v2.0"
 description       "Installs/Configures Apache Cassandra"
 long_description  IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.2.11"
+version           "0.2.12"
 
 depends           "yum", "~> 3.0"
 depends           "apt"
-depends           "java", "~> 1.15"
+depends           "java", "~> 1.22"
 depends           "sysctl", "~> 0.3"
 depends           "ntp"
 
